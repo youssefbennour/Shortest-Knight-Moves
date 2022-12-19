96c5171cf8af3c56ec4ef9c827dac7fc959e313c
youssefbennour <youssefbennour15@gmail.com>
2022-12-19 10:47:29 +0100

    adjust output format

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -4,22 +4,62 @@ using System.Drawing;
 using System.Net.Http;
 
 
-int xCoordinates, yCoordinates;
-Console.Write("Enter Target X-axis coordinates : ");
-int.TryParse(Console.ReadLine(), out xCoordinates);
 
-Console.Write("\nEnter Target Y-axis coordinates : ");
-int.TryParse(Console.ReadLine(), out yCoordinates);
+int[] knightPos = new int[2];
+int[] targetPos = new int[2];
+do {
+    Console.WriteLine("Enter target x-coordinate (starts from 0) : ");
+    int.TryParse(Console.ReadLine(), out targetPos[0]);
+} while (targetPos[0] < 0);
+do {
+    Console.WriteLine("Enter target y-coordinate (starts from 0) : ");
+    int.TryParse(Console.ReadLine(), out targetPos[1]);
+} while (targetPos[1] < 0);
 
-Knight knight = new Knight(Math.Max(xCoordinates, yCoordinates) + 5, xCoordinates, yCoordinates);
-knight.displayBoard();
+//determine grid size
+int N = Math.Max(targetPos[0], targetPos[1]) + 1;
 
-Thread.Sleep(1000);
-Console.Clear();
 
-knight.moves();
-knight.displayBoard();
+List<int> knightMoves;
 
-Console.ResetColor();
-Console.ReadKey();
+//store knight moves to the target in the following form {xCord, yCord, xCord, yCord,...}
+knightMoves = Game.minStepToReachTarget(knightPos, targetPos, N);
+
+string[,] board = new string[N, N];
+
+for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+        board[i, j] = " *";
+    }
+}
+
+//The result contains the final path printed and step enumerated from (0,0) to (target X coordinate, target Y coordinate) 
+string result = "";
+
+//inital knight cooridnates
+int x, y;
+x = 0;
+y = 0;
+
+int counter = 1;
+for (int i = 0; i < knightMoves.Count; i += 2) {
+    x += knightMoves[i];
+    y += knightMoves[i + 1];
+    board[N - y - 1, x] = counter.ToString();
+    
+    counter++;
+}
+board[N-1, 0] = " K";
+
+for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+        result += board[i, j];
+
+    }
+    result += "\n";
+}
+
+Game.convertOutputToImage(result);
+
+
 
